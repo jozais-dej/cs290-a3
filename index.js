001a0314fae4d80017dc8ba3ee8158dff90704b2
jozais-dej <72046539+jozais-dej@users.noreply.github.com>
2021-05-10 16:37:04 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -30,3 +30,45 @@ function modalToggle(event) {
     }
 }
 
+function addTwit(event) {
+    if ((twitText.value == "") || (twitAuthor.value == "")) {
+        alert("Please enter Text and Author information")
+        return;
+    }
+
+    var icon = document.createElement('i');
+    icon.classList.add('fa');
+    icon.classList.add('fa-bullhorn');
+
+    var twitIcon = document.createElement('div');
+    twitIcon.classList.add('twit-icon');
+    twitIcon.appendChild(icon);
+
+    var text = document.createElement('p');
+    text.classList.add('twit-text');
+    text.textContent = twitText.value;
+
+    var author = document.createElement('a');
+    author.href = '#';
+    author.textContent = twitAuthor.value;
+
+    var attribution = document.createElement('p');
+    attribution.classList.add('twit-attribution');
+    attribution.appendChild(author);
+
+    var twitContent = document.createElement('div');
+    twitContent.classList.add("twit-content");
+    twitContent.appendChild(text);
+    twitContent.appendChild(attribution);
+
+
+    var twit = document.createElement('article');
+    twit.classList.add('twit');
+    twit.appendChild(twitIcon);
+    twit.appendChild(twitContent);
+
+    var body = document.getElementsByClassName('twit-container')[0];
+    body.appendChild(twit);
+
+    modalToggle();
+}
