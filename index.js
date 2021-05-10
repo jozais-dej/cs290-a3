e12b07eeb9e60b706d75c9cab4ce3dc362f53555
jozais-dej <72046539+jozais-dej@users.noreply.github.com>
2021-05-10 16:39:15 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -6,27 +6,27 @@
  * Email: dejongej@oregonstate.edu
  */
 
-var button = document.getElementById("create-twit-button")
-var modalBackdrop = document.getElementById("modal-backdrop")
-var modal = document.getElementById("create-twit-modal")
-var modalCancelButton = document.getElementsByClassName("modal-cancel-button")[0]
-var modalCloseButton = document.getElementsByClassName("modal-close-button")[0]
-var modalAcceptButton = document.getElementsByClassName("modal-accept-button")[0]
-var twitText = document.getElementById("twit-text-input")
-var twitAuthor = document.getElementById("twit-attribution-input")
-var twitSearch = document.getElementById("navbar-search-input")
-var twitSearchButton = document.getElementById("navbar-search-button")
+var button = document.getElementById("create-twit-button");
+var modalBackdrop = document.getElementById("modal-backdrop");
+var modal = document.getElementById("create-twit-modal");
+var modalCancelButton = document.getElementsByClassName("modal-cancel-button")[0];
+var modalCloseButton = document.getElementsByClassName("modal-close-button")[0];
+var modalAcceptButton = document.getElementsByClassName("modal-accept-button")[0];
+var twitText = document.getElementById("twit-text-input");
+var twitAuthor = document.getElementById("twit-attribution-input");
+var twitSearch = document.getElementById("navbar-search-input");
+var twitSearchButton = document.getElementById("navbar-search-button");
 
 function modalToggle(event) {
     if (modal.classList.contains("hidden")) {
-        twitText.value = ""
-        twitAuthor.value = ""
-        modal.classList.remove('hidden')
-        modalBackdrop.classList.remove('hidden')
+        twitText.value = "";
+        twitAuthor.value = "";
+        modal.classList.remove('hidden');
+        modalBackdrop.classList.remove('hidden');
     }
     else {
-        modal.classList.add('hidden')
-        modalBackdrop.classList.add('hidden')
+        modal.classList.add('hidden');
+        modalBackdrop.classList.add('hidden');
     }
 }
 
@@ -72,3 +72,10 @@ function addTwit(event) {
 
     modalToggle();
 }
+
+button.addEventListener('click', modalToggle);
+modalCancelButton.addEventListener('click', modalToggle);
+modalCloseButton.addEventListener('click', modalToggle);
+modalAcceptButton.addEventListener('click', addTwit);
+twitSearchButton.addEventListener('click', search);
+twitSearch.addEventListener('keyup', search);
