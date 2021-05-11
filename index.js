ce420a756bafccc9fd2e649136e1ec9e51586304
jozais-dej <72046539+jozais-dej@users.noreply.github.com>
2021-05-10 18:50:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,28 +7,17 @@
  */
 
 var button = document.getElementById("create-twit-button")
+var twitText = document.getElementById("twit-text-input")
+var twitAuthor = document.getElementById("twit-attribution-input")
+var twitSearch = document.getElementById("navbar-search-input")
 var modalBackdrop = document.getElementById("modal-backdrop")
 var modal = document.getElementById("create-twit-modal")
 var modalCancelButton = document.getElementsByClassName("modal-cancel-button")[0]
 var modalCloseButton = document.getElementsByClassName("modal-close-button")[0]
 var modalAcceptButton = document.getElementsByClassName("modal-accept-button")[0]
-var twitText = document.getElementById("twit-text-input")
-var twitAuthor = document.getElementById("twit-attribution-input")
-var twitSearch = document.getElementById("navbar-search-input")
 var twitSearchButton = document.getElementById("navbar-search-button")
 
-function modalToggle(event) {
-    if (modal.classList.contains("hidden")) {
-        twitText.value = ""
-        twitAuthor.value = ""
-        modal.classList.remove('hidden')
-        modalBackdrop.classList.remove('hidden')
-    }
-    else {
-        modal.classList.add('hidden')
-        modalBackdrop.classList.add('hidden')
-    }
-}
+
 
 function addTwit(event) {
 
